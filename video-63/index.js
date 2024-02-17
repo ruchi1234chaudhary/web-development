082eba4ee1162c6a51ec0ff255301a5e7b3e0087
Ruchi Chaudhary <cruchi971@gmail.com>
2024-02-17 16:18:22 +0530

    Update index.js

--- video-63/index.js
+++ video-63/index.js
@@ -1,3 +1,24 @@
-let arr=[1,2,3,4,5]
-console.log(arr)
-console.log(arr[0])
+let a=[1,93,5,6,88]
+for (let index = 0; index < a.length; index++) {
+    const element = a[index];
+    console.log(element)
+    
+}
+//a.forEach((value,index,arr)=>{
+   // console.log(value,index,arr)
+//})
+// let obj={
+//     a:1,
+//     b:2,
+//     c:3,
+// }
+// for (const key in obj) {
+//     if (Object.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(element)
+//     }
+// }
+for (const iterator of a) {
+    console.log(iterator)
+    
+}
